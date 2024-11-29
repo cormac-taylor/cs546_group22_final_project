f04e426a077cff4230fb61ec1469b84d13f7b6d0
Vraj <patelvraj1227@gmail.com>
2024-11-28 22:07:03 -0500

    added manage games page, implemented game search with api along with basic api search functions

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 // import exampleRoutes from './example.js';
 import homeRoutes from "./home.js";
+import manageGameRoutes from "./manageGames.js"
 
 const constructorMethod = (app) => {
   //   app.use('/example', exampleRoutes);
   app.use("/", homeRoutes);
+  app.use("/manage", manageGameRoutes)
 
   app.use("*", (_, res) => {
     res.redirect("/");
