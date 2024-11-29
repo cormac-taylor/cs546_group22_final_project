e25687862d82b265dfb0f97a01459c4afa67e9d3
marcelcastillo <mcastill1@stevens.edu>
2024-11-28 21:04:02 -0500

    28 Nov: Added validation functions to data/index.js, Signup HTML, Signup Route, Hashed Password

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 // import exampleRoutes from './example.js';
 import homeRoutes from "./home.js";
+import signupRoutes from "./signup.js";
+import {static as staticDir} from 'express';
 
 const constructorMethod = (app) => {
   //   app.use('/example', exampleRoutes);
   app.use("/", homeRoutes);
+  app.use('/signup', signupRoutes);
+  app.use('/public', staticDir('public'));
 
   app.use("*", (_, res) => {
     res.redirect("/");
