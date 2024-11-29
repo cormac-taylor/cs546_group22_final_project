4530864c33f4e672901681eb1a37ae907452584d
marcelcastillo <mcastill1@stevens.edu>
2024-11-29 16:53:24 -0500

    29 Nov Session Management MidPull

--- app.js
+++ app.js
@@ -3,6 +3,37 @@ const app = express();
 import configRoutes from "./routes/index.js";
 import exphbs from "express-handlebars";
 
+/* Session Management */
+app.use(
+    session({
+        name: 'BokenBoards',
+        secret: 'Install Crypto for Better Safety lol',
+        saveUninitialized: false,
+        resave: false,
+        cookie: {maxAge: 300000}
+    })
+);
+// Verify the user is logged in before they attempt to access the dashboard route
+app.use('/dashboard', (req, res, next) => {
+    console.log(req.session.id);
+    if (!req.session.user){
+        return res.redirect('/signin');
+    } else {
+        next();
+    }
+});
+
+// If the user is already logged in and they go to the signin page:
+// reroute to the dashboard
+app.use('/signin', (req, res, next) =>{
+    if (req.session.user){
+        return res.redirect('/dashboard');
+    } else {
+        next();
+    }
+})
+
+/* Regular Express Code */
 app.use("/public", express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
