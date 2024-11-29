a6cdb8177cde8756972f75e6a839d301f28e6e57
Vraj <patelvraj1227@gmail.com>
2024-11-28 22:26:08 -0500

    fixed typos

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,15 @@
 // import exampleRoutes from './example.js';
 import homeRoutes from "./home.js";
-<<<<<<< HEAD
 import manageGameRoutes from "./manageGames.js"
-=======
 import signupRoutes from "./signup.js";
 import {static as staticDir} from 'express';
->>>>>>> main
 
 const constructorMethod = (app) => {
   //   app.use('/example', exampleRoutes);
   app.use("/", homeRoutes);
-<<<<<<< HEAD
   app.use("/manage", manageGameRoutes)
-=======
   app.use('/signup', signupRoutes);
   app.use('/public', staticDir('public'));
->>>>>>> main
 
   app.use("*", (_, res) => {
     res.redirect("/");
