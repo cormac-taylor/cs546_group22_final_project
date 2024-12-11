cce3ec07dc3667131756952e254d849eef5fe35a
cormactaylor <cormacmtaylor@gmail.com>
2024-12-11 16:45:54 -0500

    bulk of sign up done

--- public/js/client_validation.js
+++ public/js/client_validation.js
@@ -1,13 +1,30 @@
+const MIN_NAME_LEN = 2;
+const MAX_NAME_LEN = 32;
 const MIN_USERNAME_LEN = 5;
 const MAX_USERNAME_LEN = 10;
 const MIN_PASSWORD_LEN = 8;
 
-const validateStrOfLen = (str, minLen, maxLen) => {
+const validateString = (str) => {
   if (typeof str !== "string") throw "must be a string!";
   let res = str.trim();
   if (!res) throw "must not be empty!";
+  return res;
+};
+
+const validateStrOfLen = (str, minLen, maxLen) => {
+  const res = validateString(str);
   if (res.length < minLen) throw `must be at least ${minLen} chars!`;
   if (res.length > maxLen) throw `must be at most ${maxLen} chars!`;
+  return res;
+};
+
+export const validateName = (name) => {
+  // https://a-tokyo.medium.com/first-and-last-name-validation-for-forms-and-databases-d3edf29ad29d
+  const NAME_REGEX = /^[a-zA-Z]+([ \-']{0,1}[a-zA-Z]+){0,2}[.]{0,1}$/;
+
+  const res = validateStrOfLen(name, MIN_NAME_LEN, MAX_NAME_LEN);
+  //TODO: This error is going to float up to the user
+  if (!NAME_REGEX.test(res)) throw `must be of length ${MIN_NAME_LEN} to ${MAX_NAME_LEN}.`;
   return res;
 };
 
@@ -22,6 +39,12 @@ export const validateUsername = (username) => {
   ).toLowerCase();
   if (!USERNAME_REGEX.test(res))
     throw `must be username (a-z, 0-9, _, or . and between ${MIN_USERNAME_LEN} and ${MAX_USERNAME_LEN} in length)!`;
+  return res;
+};
+
+export const validateEmail = (email) => {
+  const res = validateString(email).toLowerCase();
+  if (!validator.isEmail(res)) throw "must be an email address!";
   return res;
 };
 
---
+++ public/js/signup.js
@@ -0,0 +1,88 @@
+import {
+  validateUsername,
+  validatePassword,
+  validateName,
+  validateEmail,
+} from "./client_validation.js";
+
+let signUpForm = document.getElementById("signup-form");
+let firstNameInput = document.getElementById("firstName");
+let lastNameInput = document.getElementById("lastName");
+let usernameInput = document.getElementById("username");
+let emailInput = document.getElementById("email");
+let passwordInput = document.getElementById("password");
+let confirmPasswordInput = document.getElementById("confirmPassword");
+let errorList = document.getElementById("client-error-list");
+
+if (signUpForm) {
+  signUpForm.addEventListener("submit", (event) => {
+    const errors = [];
+    const firstName = firstNameInput.value;
+    const lastName = lastNameInput.value;
+    const username = usernameInput.value;
+    const email = emailInput.value;
+    const password = passwordInput.value;
+    const confirmPassword = confirmPasswordInput.value;
+
+    try {
+      firstNameInput.value = validateName(firstName);
+    } catch (e) {
+      firstNameInput.value = firstName.trim();
+      errors.push(`First Name ${e}`);
+    }
+
+    try {
+      lastNameInput.value = validateName(lastName);
+    } catch (e) {
+      lastNameInput.value = lastName.trim();
+      errors.push(`Last Name ${e}`);
+    }
+
+    try {
+      usernameInput.value = validateUsername(username);
+    } catch (e) {
+      usernameInput.value = username.trim();
+      errors.push(`Username ${e}`);
+    }
+
+    try {
+      emailInput.value = validateEmail(email);
+    } catch (e) {
+      emailInput.value = email.trim();
+      errors.push(`Email ${e}`);
+    }
+
+    let invalidPassword = false;
+    try {
+      passwordInput.value = validatePassword(password);
+    } catch (e) {
+      passwordInput.value = password.trim();
+      invalidPassword = true;
+      errors.push(`Password ${e}`);
+    }
+
+    if (!invalidPassword) {
+      try {
+        confirmPasswordInput.value = validatePassword(confirmPassword);
+      } catch (e) {
+        confirmPasswordInput.value = confirmPassword.trim();
+        errors.push(`Confirm Password doesn't match.`);
+      }
+    }
+
+    if (errors.length > 0) {
+      event.preventDefault();
+
+      errorList.innerHTML = "";
+      for (let e of errors) {
+        let li = document.createElement("li");
+        li.innerHTML = e;
+        errorList.appendChild(li);
+      }
+      errorList.hidden = false;
+    } else {
+        errorList.innerHTML = "";
+        errorList.hidden = true;
+    }
+  });
+}
